08c9a6ee584685912a0867f6fc2bd9ed8ca00d3c
daravel <natsudragneel736@gmail.com>
2020-01-01 22:14:56 +0700

    1st Commit

---
+++ service-workers.js
@@ -0,0 +1,78 @@
+var CACHE_NAME = 'gio-cache-v1';
+var urlsToCache = [
+    '/',
+    '/index.html',
+    '/fonts/BebasKai-Regular.otf',
+    '/img/bushes.png',
+    '/img/concert.jpg',
+    '/img/people1.png',
+    '/img/people2.png',
+    '/img/people3.png',
+    '/img/sky.png',
+    '/img/water.png',
+    '/manifest.json',
+    '/style/main.css',
+    '/js/main.js'
+];
+
+self.addEventListener('install', function (event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function (cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function (response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+
+            return fetch(event.request).then(
+                function (response) {
+                    // Check if we received a valid response
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+
+                    // IMPORTANT: Clone the response. A response is a stream
+                    // and because we want the browser to consume the response
+                    // as well as the cache consuming the response, we need
+                    // to clone it so we have two streams.
+                    var responseToCache = response.clone();
+
+                    caches.open(CACHE_NAME)
+                        .then(function (cache) {
+                            cache.put(event.request, responseToCache);
+                        });
+
+                    return response;
+                }
+            );
+        })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+
+    var cacheWhitelist = CACHE_NAME;
+
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
